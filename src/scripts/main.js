b04a0867c43b95bd86ab04372681ef41e9612a56
Monogato <monogato.dg@gmail.com>
2022-08-08 17:56:23 -0500

    Create renderProductsFunction for product list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -55,3 +55,65 @@ triggerBackAsideShoppingCart.addEventListener('click', toggleBackShoppingCart);
 function toggleBackShoppingCart() {
     asideShoppingCart.classList.add('inactive');
 }
+
+//Product list  
+
+const cardsContainer = document.querySelector('.cards__container');
+
+function renderProducts(arr) {
+    for (product of arr){
+        const productCard = document.createElement('div')
+        productCard.classList.add('card')
+    
+        //product = {name, price, image} -> product.image
+        const productCardImg = document.createElement('img')
+        productCardImg.classList.add('card__image')
+        productCardImg.setAttribute('src', product.image)
+        productCardImg.setAttribute('alt', product.name)
+    
+        const productCardInfo = document.createElement('div')
+        productCardInfo.classList.add('card__info')
+    
+        const productCardInfoText = document.createElement('div')
+        productCardInfoText.classList.add('info__text') 
+        const productCardInfoTextPrice = document.createElement('p')
+        productCardInfoTextPrice.innerText = '$' + product.price
+        const productCardInfoTextName = document.createElement('p')
+        productCardInfoTextName.innerText = '$' + product.name
+    
+        const productCardInfoImage = document.createElement('figure')
+        productCardInfoImage.classList.add('info__image')
+        const productCardInfoImageImg = document.createElement('img')
+        productCardInfoImageImg.setAttribute('src', './src/static/Icons/bt_add_to_cart.svg')
+        productCardInfoImageImg.setAttribute('alt', 'add to cart')
+    
+        productCardInfoImage.appendChild(productCardInfoImageImg)
+    
+        productCardInfoText.appendChild(productCardInfoTextPrice)
+        productCardInfoText.appendChild(productCardInfoTextName)
+    
+        productCardInfo.appendChild(productCardInfoText)
+        productCardInfo.appendChild(productCardInfoImage)
+    
+        productCard.appendChild(productCardImg)
+        productCard.appendChild(productCardInfo)
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+const productList = [];
+productList.push({
+    name: 'bmx',
+    price: 120,
+    image: 'https://images.pexels.com/photos/5465162/pexels-photo-5465162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+
+})
+productList.push({
+    name: 'skate',
+    price: 35,
+    image: 'https://images.pexels.com/photos/1018483/pexels-photo-1018483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    
+})
+
+renderProducts(productList)
